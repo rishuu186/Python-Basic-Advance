3f9d988822ec1ff295a588510b828d0bd24ff73b
rishuu186 <rishuu1806@gmail.com>
2026-01-08 14:42:50 +0530

    test

---
+++ datatype.py
@@ -0,0 +1,119 @@
+# Python Data Types 
+
+
+# 1)Text Type 
+
+x = str("Hello World")
+#print(x)
+y = "Hello World"
+#print(type(y))
+
+
+
+#2) Numeric Type
+
+#Integer 
+x = int(18)
+#print(x)
+y = 18
+#print(type(y))
+
+#Floating Number
+x = float(22.0)
+#print(x)
+y = 32.3
+#print(type(y))
+
+#Complex Number
+x = complex(1j)
+#print(x)
+y = 1j
+#print(type(y))
+
+
+
+# 3) Sequence Type
+
+#list
+x = list(("apple","banana","cherry"))
+#print(x)
+y = (("apple","banana","cherry"))
+#print(type(y))
+
+#Tuple
+x = tuple(("apple","banana","cherry"))
+#print(x)
+y = (("apple","banana","cherry"))
+#print(type(y))
+
+#Range
+x = range(8)
+#print(x)
+#print(type(x))
+
+x = list(range(6))
+#print(x)
+
+
+
+# 4) Mapping Type
+
+x = dict(name="Rishita",age=21)
+#print(x)
+#print(type(x))
+
+
+
+# 5) Set Type
+
+#Set
+x = set(("apple","banana","cherry"))
+#print(x)
+y = (("apple","banana","cherry"))
+#print(type(y))
+
+#Frozenset
+x = frozenset(("apple","banana","cherry"))
+#print(x)
+y = (("apple","banana","cherry"))
+#print(type(y))
+
+
+
+
+# 6) Boolean Type
+
+x = bool(True)
+#print(x)
+y = False
+#print(type(y))
+
+
+
+# 7) Binary Types
+
+# Bytes
+x = bytes(5)
+print(x)
+print(type(x))
+
+b = b"RI"
+print(b[0])
+print(type(b))
+
+# BytesArray
+x = bytearray(3)
+#print(x)
+#print(type(x))
+
+# MemmoeryView
+x = memoryview(bytes(5))
+#print(x)
+#print(type(x))
+
+
+# 8) None Type
+
+x = None
+#print(x)
+#print(type(x))
---
+++ strings.py
@@ -0,0 +1,53 @@
+# 1) Strings
+# print("Hello")
+# print('Hello')
+
+#Quotes inside Quotes
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+#Assign string to a variable
+# a = "Hello"
+# print(a)
+
+#Multiline Strings
+# a = """This is a multiline string.
+# It can span multiple lines.
+# No need for \n manually."""
+# print(a)
+# a = '''This is a multiline string.
+# It can span multiple lines.
+# No need for \n manually.'''
+# print(a)
+
+#Strings are Arrays
+# a = "Hello,World"
+# print(a[7])
+
+#Looping Through a String
+# for x in "banana":
+#  print(x)
+
+#String Length 
+# a = "Hello,World!"
+# print(len(a))
+
+#Check string
+# txt = "The best things in life are free!"
+# print("Free" in txt)
+
+#if statement
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#     print ("yes,'Free' is present")
+
+#Check if Not
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+#if statement
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print ("No,'expensive' is not Present")
+
